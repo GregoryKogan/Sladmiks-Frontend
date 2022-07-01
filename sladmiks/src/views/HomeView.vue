506c10f7e54c10c1e4a410ddc6dc7e0e6826479c
GregoryKogan <grisha.koganovskiy@gmail.com>
2022-07-01 20:42:30 +0300

    Initial commit

---
+++ sladmiks/src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    <h1>This is homepage!</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue"; // @ is an alias to /src
+
+export default defineComponent({
+  name: "HomeView",
+  components: {},
+});
+</script>
